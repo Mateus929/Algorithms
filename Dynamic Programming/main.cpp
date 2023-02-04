a53dc7418abceb0b152bd9daa60f872782a526c4
Mateus929 <61325817+Mateus929@users.noreply.github.com>
2023-02-05 01:47:38 +0400

    Add files via upload

---
+++ Dynamic Programming/main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ff first
+#define second
+#define pb push_back
+
+using namespace std;
+
+const int INF = 1e9;
+
+int lis(int a[], int n, vector<int> &ans) {
+    ans.assign(n, INF);
+    for(int i = 0; i < n; i++) {
+        int indx = lower_bound(ans.begin(), ans.end(), a[i]) - ans.begin();
+        ans[indx] = a[i];
+    }
+    while(ans.back() == INF) {
+        ans.pop_back();
+    }
+    return ans.size();
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    vector<int> ans;
+    cout << lis(a, n, ans);
+};
