c12f93a20c3df1b37395dcbcad73d53df917a353
Mateus929 <61325817+Mateus929@users.noreply.github.com>
2023-06-17 15:16:53 +0400

    Update AVLTree.cpp

--- Data structure/AVLTree.cpp
+++ Data structure/AVLTree.cpp
@@ -76,6 +76,13 @@ private:
         // return a root
         return y;
     }
+    Node *successor(Node *node) {
+        if(node == nullptr) return node;
+        while(node -> left != nullptr) {
+            node = node -> left;
+        }
+        return node;
+    }
     Node *insert(Node *node, T val) {
         // Base case
         if(node == nullptr) {
@@ -114,19 +121,79 @@ private:
         // no rotation
         return node;
     }
+    Node *remove(Node *node, T val, bool ind = false) {
+        if(node == nullptr) return node;
+        if(val < node -> val) node -> left = remove(node -> left, val);
+        if(node -> val < val) node -> right = remove(node -> right, val);
+        if(ind && node -> left != nullptr && node -> val == val) node -> left = remove(node -> left, val);
+        else if(node -> val == val) {
+            if(node -> left == nullptr || node -> right == nullptr) {
+                Node *child = nullptr;
+                if(node -> left != nullptr) child = node -> left;
+                if(node -> right != nullptr) child = node -> right;
+                delete node;
+                node = child;
+            } else {
+                Node *child = successor(node -> right);
+                node -> val = child -> val;
+                node -> right = remove(node -> right, child -> val, true);
+            }
+        }
+        if(node == nullptr) return node;
+        updateLen(node);
+        updateHeight(node);
+        int factor = balanceFactor(node);
+        if(factor > 1 && balanceFactor(root -> left) >= 0) {
+            return rightRotation(node);
+        }
+        if(factor < -1 && balanceFactor(root -> right) <= 0) {
+            return leftRotation(node);
+        }
+        if(factor > 1) {
+            node -> left = leftRotation(node);
+            return rightRotation(node);
+        }
+        if(factor < -1) {
+            node -> right = rightRotation(node);
+            return leftRotation(node);
+        }
+        return node;
+    }
     Node *root;
     int n;
 public:
     BST() {
         root = nullptr;
+        n = 0;
     };
     ~BST() {
         deleteDFS(root);
     }
-    void insert(T val){
+    void insert(T val) {
         root = insert(root, val);
+        n++;
+    }
+    void remove(T val) {
+        if(!find(val)) return;
+        root = remove(root, val);
+        n--;
+    }
+    bool find(T val) {
+        Node *cur = root;
+        while(cur != nullptr) {
+            if(cur -> val == val) return true;
+            if(val < cur -> val) {
+                cur = cur -> left;
+            } else {
+                cur = cur -> right;
+            }
+        }
+        return false;
     }
     T findKth(int k) {
+        if(n == 0 || k <= 0) {
+            return -1;
+        }
         Node *cur = root;
         if(root -> len < k) return -1;
         while(cur -> height > 1) {
@@ -143,17 +210,29 @@ public:
         }
         return cur -> val;
     }
+    int size() {
+        return n;
+    }
+    bool isEmpty() {
+        return (n == 0);
+    }
 };
 
 const int N = 524289, mod = 998244353, INF = 1e9 + 1; // !!!
 
 int main() {
     BST<int> bt;
-    for(int i = 1; i <= 5; i++) {
+    for(int i = 1; i <= 7; i++) {
         int x;
         cin >> x;
         bt.insert(x);
     }
+    for(int i = 1; i <= 2; i++) {
+        int x;
+        cin >> x;
+        bt.remove(x);
+    }
+    cout << bt.findKth(3);
 }
 
-// remove isn't written yet
+// project has bugs and is yet to be completed
